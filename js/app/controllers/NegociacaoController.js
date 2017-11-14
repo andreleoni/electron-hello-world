a3961a0589b5c723786348214c541ed02826ab6c
André Luiz Leoni <andreluizleoni@gmail.com>
2017-11-13 23:41:39 -0200

    continuacao

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -33,20 +33,15 @@ class NegociacaoController {
     }
 
     adiciona(event) {
-        ConnectionFactory
-          .getConnection()
-          .then(connection => {
-            let negociacao = this._criaNegociacao();
-            new NegociacaoDao(connection).adiciona(negociacao)
-              .then(() => {
-                this._listaNegociacoes.adiciona(this._criaNegociacao());
-                this._mensagem.texto = 'Negociação adicionada com sucesso';
-                this._limpaFormulario();
-              })
-          })
-          .catch(erro => this._mensagem.texto = erro );
-
         event.preventDefault();
+        let negociacao = this._criaNegociacao();
+        new NegociacaoService()
+            .cadastra(negociacao)
+            .then(mensagem => {
+                this._listaNegociacoes.adiciona(negociacao);
+                this._mensagem.texto = mensagem;
+                this._limpaFormulario();
+            }).catch(erro => this._mensagem.texto = erro);
     }
 
     importaNegociacoes() {
